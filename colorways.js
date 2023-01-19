d817defb31c431791a3c5a5a8d7b1fe4baab3e7b
Remy Hunter <remyhunter256@gmail.com>
2023-01-19 14:42:37 -0500

    add drag and drop

--- colorways.js
+++ colorways.js
@@ -1,6 +1,6 @@
 import { quantize } from './quantize.js'
 
-const getPalette = (imgSrc, amtColors) => {
+const getPalette = (imgSrc, amtColors, callback) => {
     
   var canvas = document.createElement('canvas');
   var context = canvas.getContext("2d");
@@ -16,25 +16,37 @@ const getPalette = (imgSrc, amtColors) => {
   var imgData = context.getImageData(0, 0, canvas.width, canvas.height);
   let pixelData = [];
 
-  console.log(imgData.data) 
   for (var i = 0; i < imgData.data.length; i+= 4) {
       
-      // i = (y * canvas.width + x)*4;
       let r = imgData.data[0 + i],
           g = imgData.data[1 + i],
           b = imgData.data[2 + i],
           a = imgData.data[3 + i];
-      // console.log(r, g, b)
       pixelData.push([r, g, b]);
   }
   var cmap = quantize(pixelData, amtColors);
   // var newPixels = pixelData.map((p) => { return cmap.map(p); });
   // cmap = quantize(newPixels, amtColors);
   var palette = cmap.palette();
-
-  // return newPalette
+  callback();
   return palette
 }
 
+function toHex(color) {
+	let r = color[0].toString(16),
+	g = color[1].toString(16),
+	b = color[2].toString(16);
+	var result = '';
+	if (r.length == 1) {
+		r = '0' + r;
+	}
+	if (g.length == 1) {
+		g = '0' + g;
+	}
+	if (b.length == 1) {
+		b = '0' + b;
+	}
+	return '#' + r + b + g;
+} 
 
-export { getPalette };
+export { getPalette, toHex };
--- main.js
+++ main.js
@@ -1,19 +1,103 @@
-import { getPalette } from './colorways.js'
+import { getPalette, toHex } from './colorways.js'
+
+// const container = document.querySelector('#container')
+const paletteCard = document.getElementById('palette-card');
+const imgSrc = document.getElementById('src-image');
+const imgCard = document.getElementById('img-card');
+const imageInput = document.querySelector('input');
+var imgurl = 'img/sb.png'
+imgSrc.src = imgurl
+
+const uploadFile = async (event) => {
+    const file = imageInput.files[0]
+    imgurl = `${URL.createObjectURL(file)}`;
+    imgSrc.src = imgurl
+}
+
+const dragOverHandler = (e) => {
+    e.preventDefault();
+    imgCard.classList.remove('rotate')
+    imgCard.classList.remove('transition-all')
+    imgCard.classList.add('outline')
+}
+
+const dragLeaveHandler = (e) => {
+    e.preventDefault();
+    imgCard.classList.remove('outline')
+    imgCard.classList.add('rotate')
+    imgCard.classList.add('transition-all')
+}
+
+const copyToClipboard = async (text) => {
+    // try {
+    // await navigator.clipboard.writeText(text);
+    // console.log('Page URL copied to clipboard');
+    // } catch (err) {
+    // console.error('Failed to copy: ', err);
+    // }
+    // const clipboard = navigator.clipboard;
+    // clipboard.writeText(text).then(function () {
+    //   alert('module URL shared to clipboard');
+    // });
+}
+
+const dropHandler = (e) => {
+    e.preventDefault();
+    imgCard.classList.remove('outline')
+    imgCard.classList.add('transition-all')
+    const file = e.dataTransfer.files[0];
+    imgurl = `${URL.createObjectURL(file)}`;
+    imgSrc.src = imgurl
+}
+
+const drawPalette = (palette) => {
+    palette.forEach((color, c) => {
+        const hex = toHex(color);
+        console.log(color, hex)
+        const newDiv = document.createElement("div");
+        const footerLeft = document.getElementById("footer-left");
+        paletteCard.appendChild(newDiv)
+
+        // document.body.style.backgroundColor = `${'rgb(' + palette[0][0] + ', ' + palette[0][1] + ', ' + palette[0][2] + ')'}`
 
 
-const palette = getPalette('assets/sb_sm.jpg', 5)
+        newDiv.innerHTML = `
+        <div class="palette-item flex flex-row w-[100px] cursor-pointer hover:w-[300px] transition-all relative">
+            <div 
+            class="color-block w-full px-[2rem] h-[80px]" 
+            style="background-color:rgb(${color[0] + ',' + color[1] + ',' + color[2] + ')'};">
+            </div>
+            <div class="color-text select-none absolute top-[30%] translate-x-[40px] text-[1.4rem] tracking-wide flex items-center justify-center">
+                ${hex}
+            </div>
+        </div>
+        `
+    })
+}
 
-console.log(palette)
+imgSrc.onload = () => {
+    const palette = getPalette(imgurl, 4, () => {
+        imgCard.classList.remove('loading')
+        imgCard.classList.add('rotate')
+        imgSrc.classList.remove('opacity-0')
+        imgSrc.classList.add('opacity-1')
+        paletteCard.classList.remove('opacity-0')
+        paletteCard.classList.remove('loading')
+        paletteCard.classList.add('opacity-1')
+    })
 
-const container = document.querySelector('#container')
+    drawPalette(palette)
 
-palette.forEach((color, c) => {
-    const newDiv = document.createElement("div");
-    container.appendChild(newDiv)
-    newDiv.innerHTML = `
-    <div style="background-color:rgb(${color[0] + ',' + color[1]+ ',' + color[2] + ')'};">
-          *****
-    </div>
-    `
-    
-})
+    // add event listener to each item in palette
+    const paletteItems = document.getElementsByClassName('palette-item');
+    Array.from(paletteItems).forEach((block, b) => {
+
+        block.addEventListener("click", () => {
+            // TODO: copy to clipboard
+        })
+    })
+}
+
+imgCard.addEventListener("dragover", dragOverHandler);
+imgCard.addEventListener("dragleave", dragLeaveHandler);
+imgCard.addEventListener("drop", dropHandler);
